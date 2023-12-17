e71d04e7457c67ebccb91837f3e7b3165ea4aaae
satyamsahugr8 <satyamsahugr8@gmail.com>
2023-12-17 21:47:41 +0530

    current changes refer durgesh-youtube and working on @OneToOne mapping student-address

--- src/main/java/com/project/StudentManagementApplication/entity/Address.java
+++ src/main/java/com/project/StudentManagementApplication/entity/Address.java
@@ -1,11 +1,15 @@
 package com.project.StudentManagementApplication.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Address_Table")
+@Data
 public class Address {
 
     @Id
@@ -14,35 +18,8 @@ public class Address {
     private int address_id;
     private String address;
 
-    public Address() {
-        super();
-    }
-    public Address(int address_id, String address) {
-        this.address_id = address_id;
-        this.address = address;
-    }
+    @OneToOne
+    @JoinColumn(name = "student_Id")
+    private Student student;
 
-    public int getAddress_id() {
-        return address_id;
-    }
-
-    public void setAddress_id(int address_id) {
-        this.address_id = address_id;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "address_id=" + address_id +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
